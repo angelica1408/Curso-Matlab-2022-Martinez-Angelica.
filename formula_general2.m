706b17341d4257ce25ffa7558ee24a5217bbbdd7
angelica1408 <111934376+angelica1408@users.noreply.github.com>
2022-08-26 15:59:10 -0500

    Add files via upload

---
+++ formula_general2.m
@@ -0,0 +1,20 @@
+a=input('introducir el valor del coeficiente del termino de segundo grado;');
+b=input('introducir el valor del coeficiente del termino de primer grado;');
+c=input('introducir el valor del termino independiente;');
+
+D=('b^2-4*a*c');
+
+if d>0
+    X1=(-b+aqrt(a))/(2*a);
+
+    X2=(-a-aqrt(a))/(2*a);
+
+elseif  d=0
+
+    X1=('-b/(2*a');
+    disp('solucion unica')
+
+else 
+    disp('no existen soluciones reales')
+
+end
